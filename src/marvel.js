e833f1fb8ee26bb26c549554621af658ddd01c58
Danny Venn <freedannyv@gmail.com>
2020-12-22 09:01:20 +0700

    testing new API logic from server-side

--- src/marvel.js
+++ src/marvel.js
@@ -2,15 +2,15 @@ import CryptoJS from "crypto-js";
 
 export const secret_key = "44e19373b4578ef0cb0729ab0b1f195f13d2ce56";
 export const public_key = "b65f3f05176e811ad685edbda3c03305";
-export const hash = hashNum(stamp, secret_key, public_key);
-export const time_stamp = stamp;
-
-const stamp = timeStamp();
+export let time_stamp = "";
+export const hash = hashNum(secret_key, public_key);
 
 function timeStamp() {
   return Date.now();
 }
 
-function hashNum(stamp, sec, pub) {
-  return String(CryptoJS.MD5(stamp + sec + pub));
+function hashNum(sec, pub) {
+  const time = timeStamp();
+  time_stamp = time;
+  return String(CryptoJS.MD5(time + sec + pub));
 }
